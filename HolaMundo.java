0e376902171cebda1d36e6677a3452afae97b74d
unknown <mlgomezs@ut.edu.co>
2022-08-20 17:18:57 -0500

    refactor Hola mundo, add method message()

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,10 @@
 public class HolaMundo{
     public static void main(String[]args){
+        message();
 
+    }
+
+    public static void message(){
+        System.out.println("Hola mundo");
     }
 }
